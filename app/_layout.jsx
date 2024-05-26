95a5ad93d70aabd5bdb55093fb34f61634fe39fe
Samy <BrahimiSamy123@gmail.com>
2024-05-26 17:20:44 +0100

    first

---
+++ app/_layout.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import { Stack } from 'expo-router'
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }} />
+    </Stack>
+  )
+}
+
+export default RootLayout
---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import { Link } from 'expo-router'
+export default function App() {
+  return (
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='text-3xl'>Leaf disease classification</Text>
+      <StatusBar style='auto' />
+      <Link href='/home' style={{ color: 'blue' }}>
+        Go to home
+      </Link>
+    </View>
+  )
+}
